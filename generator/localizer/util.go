0e98e35809d18e51acfcc33d6298d3d7e3490f6e
Doridian <git@doridian.net>
2023-12-19 22:41:35 -0800

    Test IPNetAdd

--- generator/localizer/util.go
+++ generator/localizer/util.go
@@ -2,6 +2,12 @@ package localizer
 
 import "net"
 
+// IPNetAdd returns the result of adding the hostIP and remoteIP together, using the subnet as a mask.
+// For example, assume:
+// subnet = */16
+// hostIP = 0.0.0.3
+// remoteIP = 10.2.3.4
+// This will yield the IP 10.2.0.3
 func IPNetAdd(subnet *net.IPNet, hostIP net.IP, remoteIP net.IP) net.IP {
 	mask := subnet.Mask
 	n := len(hostIP)
