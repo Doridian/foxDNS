df8d55f3f6490fa050cb3cd99963f90353e8e3dc
Doridian <git@doridian.net>
2023-12-24 12:24:28 -0800

    Use faketime to test cache aging out

--- generator/resolver/base_test.go
+++ generator/resolver/base_test.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"net"
 	"testing"
+	"time"
 
 	"github.com/Doridian/foxDNS/generator"
 	"github.com/Doridian/foxDNS/generator/resolver"
@@ -12,29 +13,37 @@ import (
 	"github.com/Doridian/foxDNS/server"
 	"github.com/miekg/dns"
 	"github.com/stretchr/testify/assert"
+	"github.com/tkuchiki/faketime"
 )
 
-const dummyZone = "$TTL 300\n@ IN SOA ns1.example.com. hostmaster.example.com. 1 3600 900 604800 300\n@ IN NS ns1.example.com.\n@ IN NS ns2.example.com.\n@ IN A 10.13.37.0"
+const dummyZone = "$TTL 5\n@ IN SOA ns1.example.com. hostmaster.example.com. 1 3600 900 604800 300\n@ IN NS ns1.example.com.\n@ IN NS ns2.example.com.\n@ IN A 10.13.37.0"
+const emptyZone = "$TTL 5\n@ IN SOA ns1.example.com. hostmaster.example.com. 1 3600 900 604800 300\n@ IN NS ns1.example.com.\n@ IN NS ns2.example.com."
 
 var dummyServer *server.Server
 var resolverGenerator *resolver.Generator
 var simpleHandler *simple.Generator
 
+func loadSimpleZone(zone string) *simple.Generator {
+	staticHandler := static.New(false)
+	err := staticHandler.LoadZone(bytes.NewReader([]byte(zone)), "example.com.db", "example.com.", 300, false)
+	if err != nil {
+		panic(err)
+	}
+	simpleHandlerMake := simple.New("example.com.")
+	simpleHandlerMake.Child = staticHandler
+	return simpleHandlerMake
+}
+
 func initTests() {
 	if dummyServer != nil {
+		dummyServer.SetHandler(simpleHandler)
 		resolverGenerator.FlushCache()
 		return
 	}
 
 	dummyServer = server.NewServer([]string{"127.0.0.1:12053"}, false)
 
-	staticHandler := static.New(false)
-	err := staticHandler.LoadZone(bytes.NewReader([]byte(dummyZone)), "example.com.db", "example.com.", 300, false)
-	if err != nil {
-		panic(err)
-	}
-	simpleHandler = simple.New("example.com.")
-	simpleHandler.Child = staticHandler
+	simpleHandler = loadSimpleZone(dummyZone)
 
 	dummyServer.SetHandler(simpleHandler)
 	go dummyServer.Serve()
@@ -45,7 +54,7 @@ func initTests() {
 		},
 	})
 	dummyServer.WaitReady()
-	err = resolverGenerator.Start()
+	err := resolverGenerator.Start()
 	if err != nil {
 		panic(err)
 	}
@@ -79,7 +88,7 @@ func TestExistingRecord(t *testing.T) {
 				Name:     "example.com.",
 				Rrtype:   dns.TypeA,
 				Class:    dns.ClassINET,
-				Ttl:      300,
+				Ttl:      5,
 				Rdlength: 4,
 			},
 			A: net.ParseIP("10.13.37.0").To4(),
@@ -103,7 +112,7 @@ func TestNonExistingRecord(t *testing.T) {
 				Name:     "example.com.",
 				Rrtype:   dns.TypeSOA,
 				Class:    dns.ClassINET,
-				Ttl:      300,
+				Ttl:      5,
 				Rdlength: 61,
 			},
 			Ns:      "ns1.example.com.",
@@ -132,7 +141,7 @@ func TestNXDOMAIN(t *testing.T) {
 				Name:     "example.com.",
 				Rrtype:   dns.TypeSOA,
 				Class:    dns.ClassINET,
-				Ttl:      300,
+				Ttl:      5,
 				Rdlength: 61,
 			},
 			Ns:      "ns1.example.com.",
@@ -147,6 +156,12 @@ func TestNXDOMAIN(t *testing.T) {
 }
 
 func TestExistingRecordWithCache(t *testing.T) {
+	emptyZoneHandler := loadSimpleZone(emptyZone)
+
+	timeBegin := time.Now()
+	fakedTime := faketime.NewFaketimeWithTime(timeBegin)
+	fakedTime.Do()
+
 	testWriter := &generator.TestResponseWriter{}
 	qmsg := &dns.Msg{
 		Question: []dns.Question{
@@ -168,7 +183,7 @@ func TestExistingRecordWithCache(t *testing.T) {
 				Name:     "example.com.",
 				Rrtype:   dns.TypeA,
 				Class:    dns.ClassINET,
-				Ttl:      300,
+				Ttl:      5,
 				Rdlength: 4,
 			},
 			A: net.ParseIP("10.13.37.0").To4(),
@@ -176,8 +191,8 @@ func TestExistingRecordWithCache(t *testing.T) {
 	}, response.Answer)
 	assert.ElementsMatch(t, []dns.RR{}, response.Ns)
 
-	// Shut down backend resolver to make sure cache is required
-	dummyServer.SetHandler(nil)
+	// Empty out zone such that any returned A record must come from cache
+	dummyServer.SetHandler(emptyZoneHandler)
 
 	testWriter = &generator.TestResponseWriter{}
 	resolverGenerator.ServeDNS(testWriter, qmsg)
@@ -191,7 +206,7 @@ func TestExistingRecordWithCache(t *testing.T) {
 				Name:     "example.com.",
 				Rrtype:   dns.TypeA,
 				Class:    dns.ClassINET,
-				Ttl:      300,
+				Ttl:      5,
 				Rdlength: 4,
 			},
 			A: net.ParseIP("10.13.37.0").To4(),
@@ -199,6 +214,37 @@ func TestExistingRecordWithCache(t *testing.T) {
 	}, response.Answer)
 	assert.ElementsMatch(t, []dns.RR{}, response.Ns)
 
-	// And re-add the backend for future tests
-	dummyServer.SetHandler(simpleHandler)
+	fakedTime.Undo()
+
+	// Fake time 6 secodns ahead to force record to be uncached
+	fakedTime = faketime.NewFaketimeWithTime(timeBegin.Add(6 * time.Second))
+	fakedTime.Do()
+
+	testWriter = &generator.TestResponseWriter{}
+	resolverGenerator.ServeDNS(testWriter, qmsg)
+
+	assert.True(t, testWriter.HadWrites)
+	response = testWriter.LastMsg
+	assert.Equal(t, dns.RcodeSuccess, response.Rcode)
+	assert.ElementsMatch(t, []dns.RR{}, response.Answer)
+	assert.ElementsMatch(t, []dns.RR{
+		&dns.SOA{
+			Hdr: dns.RR_Header{
+				Name:     "example.com.",
+				Rrtype:   dns.TypeSOA,
+				Class:    dns.ClassINET,
+				Ttl:      5,
+				Rdlength: 61,
+			},
+			Ns:      "ns1.example.com.",
+			Mbox:    "hostmaster.example.com.",
+			Serial:  1,
+			Refresh: 3600,
+			Retry:   900,
+			Expire:  604800,
+			Minttl:  300,
+		},
+	}, response.Ns)
+
+	fakedTime.Undo()
 }
--- go.mod
+++ go.mod
@@ -8,10 +8,12 @@ require (
 	github.com/miekg/dns v1.1.57
 	github.com/prometheus/client_golang v1.17.0
 	github.com/stretchr/testify v1.8.4
+	github.com/tkuchiki/faketime v0.1.1
 	gopkg.in/yaml.v3 v3.0.1
 )
 
 require (
+	bou.ke/monkey v1.0.2 // indirect
 	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/cespare/xxhash/v2 v2.2.0 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
