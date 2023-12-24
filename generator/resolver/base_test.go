4040d81d9e8aa02f499408ebb20fa0862225aab0
Doridian <git@doridian.net>
2023-12-24 11:38:48 -0800

    test cache somewhat

--- generator/resolver/base_test.go
+++ generator/resolver/base_test.go
@@ -16,37 +16,42 @@ import (
 
 const dummyZone = "$TTL 300\n@ IN SOA ns1.example.com. hostmaster.example.com. 1 3600 900 604800 300\n@ IN NS ns1.example.com.\n@ IN NS ns2.example.com.\n@ IN A 10.13.37.0"
 
-func makeResolver() (*resolver.Generator, *server.Server) {
-	dummyServer := server.NewServer([]string{"127.0.0.1:12053"}, false)
+var dummyServer *server.Server
+var resolverGenerator *resolver.Generator
+var simpleHandler *simple.Generator
+
+func initTests() {
+	if dummyServer != nil {
+		return
+	}
+
+	dummyServer = server.NewServer([]string{"127.0.0.1:12053"}, false)
 
 	staticHandler := static.New(false)
 	staticHandler.LoadZone(bytes.NewReader([]byte(dummyZone)), "example.com.db", "example.com.", 300, false)
-	simpleHandler := simple.New("example.com.")
+	simpleHandler = simple.New("example.com.")
 	simpleHandler.Child = staticHandler
 
 	dummyServer.SetHandler(simpleHandler)
 	go dummyServer.Serve()
-	g := resolver.New([]*resolver.ServerConfig{
+	resolverGenerator = resolver.New([]*resolver.ServerConfig{
 		{
 			Addr:  "127.0.0.1:12053",
 			Proto: "udp",
 		},
 	})
 	dummyServer.WaitReady()
-	return g, dummyServer
+	resolverGenerator.Start()
 }
 
 func queryResolver(t *testing.T, q dns.Question) *dns.Msg {
-	g, dummyServer := makeResolver()
-	g.Start()
-	defer dummyServer.Shutdown()
-	defer g.Stop()
+	initTests()
 
 	testWriter := &generator.TestResponseWriter{}
 	qmsg := &dns.Msg{
 		Question: []dns.Question{q},
 	}
-	g.ServeDNS(testWriter, qmsg)
+	resolverGenerator.ServeDNS(testWriter, qmsg)
 
 	assert.True(t, testWriter.HadWrites)
 
@@ -132,4 +137,61 @@ func TestNXDOMAIN(t *testing.T) {
 			Minttl:  300,
 		},
 	}, response.Ns)
+}
+
+func TestExistingRecordWithCache(t *testing.T) {
+	testWriter := &generator.TestResponseWriter{}
+	qmsg := &dns.Msg{
+		Question: []dns.Question{
+			{
+				Name:   "example.com.",
+				Qtype:  dns.TypeA,
+				Qclass: dns.ClassINET,
+			},
+		},
+	}
+	resolverGenerator.ServeDNS(testWriter, qmsg)
+
+	assert.True(t, testWriter.HadWrites)
+	response := testWriter.LastMsg
+	assert.Equal(t, dns.RcodeSuccess, response.Rcode)
+	assert.ElementsMatch(t, []dns.RR{
+		&dns.A{
+			Hdr: dns.RR_Header{
+				Name:     "example.com.",
+				Rrtype:   dns.TypeA,
+				Class:    dns.ClassINET,
+				Ttl:      300,
+				Rdlength: 4,
+			},
+			A: net.ParseIP("10.13.37.0").To4(),
+		},
+	}, response.Answer)
+	assert.ElementsMatch(t, []dns.RR{}, response.Ns)
+
+	// Shut down backend resolver to make sure cache is required
+	dummyServer.SetHandler(nil)
+
+	testWriter = &generator.TestResponseWriter{}
+	resolverGenerator.ServeDNS(testWriter, qmsg)
+
+	assert.True(t, testWriter.HadWrites)
+	response = testWriter.LastMsg
+	assert.Equal(t, dns.RcodeSuccess, response.Rcode)
+	assert.ElementsMatch(t, []dns.RR{
+		&dns.A{
+			Hdr: dns.RR_Header{
+				Name:     "example.com.",
+				Rrtype:   dns.TypeA,
+				Class:    dns.ClassINET,
+				Ttl:      300,
+				Rdlength: 4,
+			},
+			A: net.ParseIP("10.13.37.0").To4(),
+		},
+	}, response.Answer)
+	assert.ElementsMatch(t, []dns.RR{}, response.Ns)
+
+	// And re-add the backend for future tests
+	dummyServer.SetHandler(simpleHandler)
 }
