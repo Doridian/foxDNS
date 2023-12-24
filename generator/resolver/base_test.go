e5ce9d2caca07a8501068787bf05463932b8e4d2
Doridian <git@doridian.net>
2023-12-24 12:37:43 -0800

    Test AllowOnlyFromPrivate

--- generator/resolver/base_test.go
+++ generator/resolver/base_test.go
@@ -36,6 +36,7 @@ func initTests() {
 	if dummyServer != nil {
 		dummyServer.SetHandler(simpleHandler)
 		resolverGenerator.FlushCache()
+		resolverGenerator.AllowOnlyFromPrivate = false
 		return
 	}
 
@@ -151,4 +152,106 @@ func TestNXDOMAIN(t *testing.T) {
 			Minttl:  300,
 		},
 	}, response.Ns)
+}
+
+func TestAllowOnlyPrivateFalseFromPublic(t *testing.T) {
+	initTests()
+	resolverGenerator.AllowOnlyFromPrivate = false
+
+	testWriter := &generator.TestResponseWriter{
+		RemoteAddrVal: &net.UDPAddr{
+			IP:   net.ParseIP("8.8.8.8").To4(),
+			Port: 12345,
+		},
+	}
+	qmsg := &dns.Msg{
+		Question: []dns.Question{
+			{
+				Name:   "example.com.",
+				Qtype:  dns.TypeA,
+				Qclass: dns.ClassINET,
+			},
+		},
+	}
+	resolverGenerator.ServeDNS(testWriter, qmsg)
+
+	assert.Equal(t, dns.RcodeSuccess, testWriter.LastMsg.Rcode)
+	assert.ElementsMatch(t, []dns.RR{
+		&dns.A{
+			Hdr: dns.RR_Header{
+				Name:     "example.com.",
+				Rrtype:   dns.TypeA,
+				Class:    dns.ClassINET,
+				Ttl:      5,
+				Rdlength: 4,
+			},
+			A: net.ParseIP("10.13.37.0").To4(),
+		},
+	}, testWriter.LastMsg.Answer)
+	assert.ElementsMatch(t, []dns.RR{}, testWriter.LastMsg.Ns)
+}
+
+func TestAllowOnlyPrivateTrueFromPublic(t *testing.T) {
+	initTests()
+	resolverGenerator.AllowOnlyFromPrivate = true
+
+	testWriter := &generator.TestResponseWriter{
+		RemoteAddrVal: &net.UDPAddr{
+			IP:   net.ParseIP("8.8.8.8").To4(),
+			Port: 12345,
+		},
+	}
+	qmsg := &dns.Msg{
+		Question: []dns.Question{
+			{
+				Name:   "example.com.",
+				Qtype:  dns.TypeA,
+				Qclass: dns.ClassINET,
+			},
+		},
+	}
+
+	resolverGenerator.ServeDNS(testWriter, qmsg)
+
+	assert.Equal(t, dns.RcodeRefused, testWriter.LastMsg.Rcode)
+	assert.Empty(t, testWriter.LastMsg.Answer)
+	assert.Empty(t, testWriter.LastMsg.Ns)
+}
+
+func TestAllowOnlyPrivateTrueFromPrivate(t *testing.T) {
+	initTests()
+	resolverGenerator.AllowOnlyFromPrivate = true
+
+	testWriter := &generator.TestResponseWriter{
+		RemoteAddrVal: &net.UDPAddr{
+			IP:   net.ParseIP("10.20.30.40").To4(),
+			Port: 12345,
+		},
+	}
+	qmsg := &dns.Msg{
+		Question: []dns.Question{
+			{
+				Name:   "example.com.",
+				Qtype:  dns.TypeA,
+				Qclass: dns.ClassINET,
+			},
+		},
+	}
+
+	resolverGenerator.ServeDNS(testWriter, qmsg)
+
+	assert.Equal(t, dns.RcodeSuccess, testWriter.LastMsg.Rcode)
+	assert.ElementsMatch(t, []dns.RR{
+		&dns.A{
+			Hdr: dns.RR_Header{
+				Name:     "example.com.",
+				Rrtype:   dns.TypeA,
+				Class:    dns.ClassINET,
+				Ttl:      5,
+				Rdlength: 4,
+			},
+			A: net.ParseIP("10.13.37.0").To4(),
+		},
+	}, testWriter.LastMsg.Answer)
+	assert.ElementsMatch(t, []dns.RR{}, testWriter.LastMsg.Ns)
 }
