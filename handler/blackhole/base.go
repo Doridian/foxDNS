1426cc788cfe47daf5e8df592994b678e3c96db8
Doridian <git@doridian.net>
2025-07-20 21:17:14 -0700

    Return SOA

--- handler/blackhole/base.go
+++ handler/blackhole/base.go
@@ -6,21 +6,41 @@ import (
 	"github.com/miekg/dns"
 )
 
+const BlackholeHost = "blackhole.foxdns.doridian.net."
+
 type Generator struct {
-	reason string
+	soa   []dns.RR
+	edns0 []dns.EDNS0
 }
 
 func New(reason string) *Generator {
 	return &Generator{
-		reason: reason,
+		soa: []dns.RR{
+			&dns.SOA{
+				Hdr: dns.RR_Header{
+					Name:   BlackholeHost,
+					Rrtype: dns.TypeSOA,
+					Class:  dns.ClassINET,
+					Ttl:    3600,
+				},
+				Ns:      BlackholeHost,
+				Mbox:    BlackholeHost,
+				Serial:  666,
+				Refresh: 3600,
+				Retry:   3600,
+				Expire:  3600,
+				Minttl:  3600,
+			},
+		},
+		edns0: []dns.EDNS0{&dns.EDNS0_EDE{
+			InfoCode:  dns.ExtendedErrorCodeFiltered,
+			ExtraText: reason,
+		}},
 	}
 }
 
 func (r *Generator) HandleQuestion(q *dns.Question, _ bool, _ net.IP) (recs []dns.RR, ns []dns.RR, edns0Opts []dns.EDNS0, rcode int) {
-	return nil, nil, []dns.EDNS0{&dns.EDNS0_EDE{
-		InfoCode:  dns.ExtendedErrorCodeFiltered,
-		ExtraText: r.reason,
-	}}, dns.RcodeNameError
+	return nil, r.soa, r.edns0, dns.RcodeNameError
 }
 
 func (r *Generator) GetName() string {
